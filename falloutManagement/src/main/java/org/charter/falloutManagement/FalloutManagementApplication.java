6388771d0edccd24bec02b6bd626f2ec93702be7
sriharsha-vankamamidi <sriharsha.vankamamidi@gmail.com>
2020-04-18 11:40:34 -0500

    first commit

---
+++ falloutManagement/src/main/java/org/charter/falloutManagement/FalloutManagementApplication.java
@@ -0,0 +1,13 @@
+package org.charter.falloutManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalloutManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalloutManagementApplication.class, args);
+	}
+
+}
---
+++ falloutManagement/src/test/java/org/charter/falloutManagement/FalloutManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.charter.falloutManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalloutManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
